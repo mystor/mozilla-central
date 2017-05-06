94ca6ad825f88b6e7d6cb7465722247780d6f80d
Michael Layzell <michael@thelayzells.com>
2017-05-05 23:07:25 -0400

    Bug 1293362 - Add a test for implementing nsIRunnable in Rust and calling the interface from C++, r=froydnj
    MozReview-Commit-ID: I23G6rmAeDN

--- xpcom/rust/xpcom/gtest/test.rs
+++ xpcom/rust/xpcom/gtest/test.rs
@@ -1,9 +1,13 @@
 #![allow(non_snake_case)]
 
+#[macro_use]
 extern crate xpcom;
 
+extern crate nserror;
+
 use std::ptr;
-use xpcom::RefPtr;
+use xpcom::{RefPtr, XpCom};
+use nserror::{nsresult, NS_OK};
 
 const NS_IOSERVICE_CID: xpcom::nsCID =
     xpcom::nsID(0x9ac9e770, 0x18bc, 0x11d3,
@@ -19,4 +23,28 @@ pub unsafe extern fn Rust_CallIURIFromRust() -> bool {
     assert!(io_svc.QueryInterface::<xpcom::nsISupports>().unwrap().is_some());
     assert!(io_svc.QueryInterface::<xpcom::nsIURI>().is_err());
     true
+}
+
+#[no_mangle]
+pub unsafe extern fn Rust_ImplementRunnableInRust(it_worked: *mut bool,
+                                                  runnable: *mut *const xpcom::nsIRunnable) {
+    // Define a type which implements nsIRunnable in rust.
+    #[derive(xpcom)]
+    #[xpimplements(nsIRunnable)]
+    struct InitMyRunnable {
+        it_worked: *mut bool,
+    }
+
+    impl MyRunnable {
+        unsafe fn run(&self) -> nsresult {
+            *self.it_worked = true;
+            NS_OK
+        }
+    }
+
+    // Create my runnable type, and forget it into the outparameter!
+    let my_runnable = MyRunnable::allocate(InitMyRunnable {
+        it_worked: it_worked
+    });
+    my_runnable.QueryInterface(&xpcom::nsIRunnable::iid(), runnable as *mut _);
 }
